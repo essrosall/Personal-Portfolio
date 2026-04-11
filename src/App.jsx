f64a24403c2e136b7d2d6c8608ce102760658bbf
essrosall <rosales.johnrey.agpalza@gmail.com>
2026-04-12 00:08:15 +0800

    Mofied the App.jsx file from src folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import {Hero} from "@/components/Hero";
+import {About} from "@/components/About";
+import {Experience} from "@/components/Experience";
+import {Projects} from "@/components/Projects";
+import {Contact} from "@/components/Contact";
+import {Navbar} from "@/layout/navbar";
+
 function App() {
-  return<></>
+  return<div className="min-h-screen overflow-x-hidden">
+    <Navbar />
+    <main>
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </main>
+    </div>
   
 }
 
