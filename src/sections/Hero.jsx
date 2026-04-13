f79cacb4d4e9989788e8596ac3113a89133554d9
essrosall <rosales.johnrey.agpalza@gmail.com>
2026-04-13 21:30:03 +0800

    "Added a floating animation to the hero section and updated the styling of the index.css file to enhance the overall design of the website."

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -140,6 +140,32 @@ export const Hero = () => {
             </div>  
           </div>
           {/*right Column - Profile Image*/}
+          <div className="relatice animate-fade-in animation-delay-400">
+            {/*Profile Image*/}
+            <div className="relative max-w-md mx-auto">
+              <div className="absolute inset-0 
+              rounded-3xl bg-gradient-tobr 
+              from-[var(--color-primary)]/30 
+              via-transparent to-[var(--color-primary)]/10 
+              blur-2xl animate-pulse-glow"/>
+              <div className="relative glass rounded-3xl p-2 glow-border">
+                <img src="" alt="John Rey Rosales" className="w-full aspect-[4/5] objext-cover rounded-2xl"/>
+                {/*Floating badge*/}
+                <div className="absolute -bottom-4 -right-4 glass rounded-xl px-4 py-3 animate-float ">
+                  <div className="flex items-center gap-3">
+                    <div className="w-3 h-3 bg-green-500 rounded-full animate-pulse">
+                      <span className="text-sm font-medium">Available for work</span>
+                    </div>
+                  </div>
+                </div>
+                {/*Stats Badge*/}
+                <div className="absolute -top-4 -left-4 glass rounded-xl px-4 py-3 animate-float animation-delay-500">
+                  <div className="text-xl font-bold text-[var(--color-primary)]">1 </div>
+                  <div className="text-xs text-[var(--color-muted-foreground)]">Year Experience</div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
